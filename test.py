e78c4859c9b2a6ac870f6d21a4e2367fd278cbea
legop3 <minecratfd8@gmail.com>
2024-04-02 00:51:06 -0400

    i  hate CORS

--- test.py
+++ test.py
@@ -16,25 +16,25 @@ while True:
         # up
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi")
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=1")
-        os.system('curl -k "http://admin:@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=1"')
+        os.system('curl -k "http://admin:wormy@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=1"')
     if rand == 1:
         # down
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi")
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=7")
-        os.system('curl -k "http://admin:@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=7"')
+        os.system('curl -k "http://admin:wormy@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=7"')
     if rand == 2: 
         # left
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi")
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=3")
-        os.system('curl -k "http://admin:@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=3"')
+        os.system('curl -k "http://admin:wormy@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=3"')
     if rand == 3:
         # right
         # r = requests.post("http://192.168.0.20/pantiltcontrol.cgi", headers=headers, params={'PanSingleMoveDegree': '5', 'TiltSingleMoveDegree': '5', 'PanTiltSingleMove': '5'})
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=5")
-        os.system('curl -k "http://admin:@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=5"')
+        os.system('curl -k "http://admin:wormy@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=5"')
     if rand == 4:
         # home
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi", params={'PanSingleMoveDegree': 5, 'TiltSingleMoveDegree': 5, 'PanTiltSingleMove': 4})
         # r = requests.get("http://192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=5&TiltSingleMoveDegree=5&PanTiltSingleMove=4")
-        os.system('curl -k "http://admin:@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=4"')
+        os.system('curl -k "http://admin:wormy@192.168.0.20/pantiltcontrol.cgi?PanSingleMoveDegree=30&TiltSingleMoveDegree=30&PanTiltSingleMove=4"')
     time.sleep(30)
